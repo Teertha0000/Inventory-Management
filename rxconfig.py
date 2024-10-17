a34f0fe0341da8fd0bf5ee34643ab210cbd4a0cd
Teertha Debnath <tirthadebnath719@gmail.com>
2024-10-17 21:08:35 +0600

    db_fix

--- rxconfig.py
+++ rxconfig.py
@@ -3,5 +3,5 @@ import os
 
 config = rx.Config(
     app_name="inventorymanagement",
-    db_url=os.environ.get("DB_URL", "postgresql://Inventory_Management_owner:S3fTU8YHzFko@ep-dawn-snow-a1o89k9i-pooler.ap-southeast-1.aws.neon.tech/Inventory_Management?sslmode=require")
+    db_url=os.environ.get("DB_URL")
     )
